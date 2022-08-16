a0b5863ad3fb8ae302e96221cc03a450a604dea5
chaitra1403 <58132646+chaitra1403@users.noreply.github.com>
2022-08-16 22:22:38 +0530

    Add files via upload
    A1 --> Given a list, find duplicates
    A2 --> Using recursion, find/print prime numbers between 2 input numbers (in a range)

---
+++ A1.py
@@ -0,0 +1,9 @@
+ent=list(map(int,input("Enter list elements").split(" ")))
+newdict={}
+for i in range(len(ent)):
+	if ent[i] in newdict:
+		if(newdict.get(ent[i])==1):
+			newdict[ent[i]]+=1
+			print(ent[i])
+	else:
+		newdict[ent[i]]=1
